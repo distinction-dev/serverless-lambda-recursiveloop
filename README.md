5bc84b82ceb7ee5c5f4364c30c152c4112c1af36
Nilay <nilay.parmar@wickes.co.uk>
2024-09-02 22:36:08 +0530

    feat: :sparkles: create plugin for recursiveLoop lambda property

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# serverless-lambda-recursiveloop
+## Getting started
+
+### Installation
+
+`npm install --save-dev @distinction-dev/serverless-lambda-recursiveloop`
+
+# Serverless RecursiveLoop Plugin
+
+A Serverless Framework plugin that adds a `RecursiveLoop` property to AWS Lambda function configurations. This property controls the recursive loop detection behavior for Lambda functions.
+
+## Functionality
+
+The `RecursiveLoop` property allows you to configure how AWS Lambda handles recursive loop detections for your functions. 
+
+- **Allowed Values**:
+  - `Allow`: No action is taken if a recursive loop is detected.
+  - `Terminate`: The function invocation is stopped, and a notification is sent when a recursive loop is detected.
+
+- **Type**: String
+- **Required**: No
+- **Resource Type**: `AWS::Lambda::Function`
+
+## Installation
+
+To use this plugin in your Serverless project, follow these steps:
+
+1. **Create the Plugin File**:
+   Create a file named `serverless-recursive-loop-plugin.js` in the root directory of your Serverless project and paste the plugin code into it.
+
+2. **Add the Plugin to Your Serverless Configuration**:
+   In your `serverless.yml` file, add the plugin under the `plugins` section.
+
+   ```yaml
+   service: my-service
+
+   provider:
+     name: aws
+     runtime: nodejs14.x
+
+   plugins:
+     - "@distinction-dev/serverless-lambda-recursiveloop"
+
+   functions:
+     myFunction:
+       handler: handler.myFunction
+       recursiveLoop: Terminate
+
+     anotherFunction:
+       handler: handler.anotherFunction
+       recursiveLoop: Allow
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * Serverless Plugin to add a `RecursiveLoop` property to AWS Lambda functions.
+ * 
+ * The `RecursiveLoop` property allows configuration of recursive loop detection
+ * for Lambda functions. When set to "Allow", no action is taken if a loop is detected.
+ * When set to "Terminate", the function invocation is stopped and a notification is sent.
+ * 
+ * @example
+ * // serverless.yml configuration
+ * functions:
+ *   myFunction:
+ *     handler: handler.myFunction
+ *     recursiveLoop: Terminate
+ */
+
+class ServerlessRecursiveLoopPlugin {
+  constructor(serverless) {
+    this.serverless = serverless;
+    this.provider = this.serverless.getProvider("aws");
+    this.hooks = {
+      "package:compileFunctions": this.addRecursiveLoopProperty.bind(this),
+    };
+  }
+
+  /**
+   * Adds the `RecursiveLoop` property to the Lambda function's CloudFormation template.
+   *
+   * This method iterates through all functions defined in the Serverless service.
+   * If a function has a `recursiveLoop` property, it validates the value and adds
+   * it to the CloudFormation template.
+   */
+  addRecursiveLoopProperty() {
+    const service = this.serverless.service;
+    for (const functionName in service.functions) {
+      const func = service.functions[functionName];
+      if (func.recursiveLoop) {
+        const recursiveLoop = func.recursiveLoop;
+        if (["Allow", "Terminate"].includes(recursiveLoop)) {
+          const functionResource =
+            service.provider.compiledCloudFormationTemplate.Resources[
+              this.provider.naming.getLambdaLogicalId(functionName)
+            ];
+          functionResource.Properties.RecursiveLoop = recursiveLoop;
+        } else {
+          this.serverless.cli.log(
+            `Invalid RecursiveLoop value in function ${functionName}. Allowed values are 'Allow' or 'Terminate'.`
+          );
+        }
+      }
+    }
+  }
+}
+
+module.exports = ServerlessRecursiveLoopPlugin;
